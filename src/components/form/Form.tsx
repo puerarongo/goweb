a55eef832251df5dcd1fcec4817898a28843b928
puerarongo <akhardin96@gmail.com>
2023-01-22 17:44:57 +0200

    add modal for form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -3,84 +3,91 @@ import styles from "./Form.module.css";
 import svgPath from "../../services/svgPath";
 import { Formik } from "formik";
 import formValidationSchema from "../../services/formValidationSchema";
+import Modal from "./modal/Modal";
 
 const Form: React.FC = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const [tumbler, setTumbler] = useState<boolean>(false);
 
   return (
-    <section className={styles.section__form} id="contact">
-      <div className={styles.image}></div>
-      <Formik
-        initialValues={{
-          name: name,
-          email: email,
-        }}
-        validationSchema={formValidationSchema}
-        onSubmit={(values) => {
-          const { name, email } = values;
-          console.log(name, email);
-          setEmail(name);
-          setName(email);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.container__into}>
-              <h2 className={styles.title}>Request Callback</h2>
-              <input
-                className={styles.input__name}
-                type="name"
-                placeholder="Enter your name"
-                name="name"
-                value={values.name}
-                onBlur={handleBlur}
-                onChange={handleChange}
-              />
-              {errors.name && touched.name ? (
-                <div className={styles.error__container}>
-                  <svg className={styles.worning__svg}>
-                    <use href={svgPath.worning + "#worning"}></use>
-                  </svg>
-                  <p className={styles.error}>{`${errors.email}`}</p>
-                </div>
-              ) : (
-                <span className={styles.default}></span>
-              )}
-              <input
-                type="email"
-                className={styles.input__email}
-                placeholder="Enter email*"
-                name="email"
-                value={values.email}
-                onBlur={handleBlur}
-                onChange={handleChange}
-              />
-              {errors.email && touched.email ? (
-                <div className={styles.error__container}>
-                  <svg className={styles.worning__svg}>
-                    <use href={svgPath.worning + "#worning"}></use>
-                  </svg>
-                  <p className={styles.error}>{`${errors.email}`}</p>
-                </div>
-              ) : (
-                <span className={styles.default}></span>
-              )}
-              <button type="submit" className={styles.from__button}>
-                Send
-              </button>
-            </div>
-          </form>
-        )}
-      </Formik>
-    </section>
+    <>
+      {tumbler && <Modal setTumbler={setTumbler} name={name} email={email} />}
+      <section className={styles.section__form} id="contact">
+        <div className={styles.image}></div>
+        <Formik
+          initialValues={{
+            name: name,
+            email: email,
+          }}
+          validationSchema={formValidationSchema}
+          onSubmit={(values) => {
+            const { name, email } = values;
+            values.name = "";
+            values.email = "";
+            setEmail(name);
+            setName(email);
+            setTumbler(true);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <div className={styles.container__into}>
+                <h2 className={styles.title}>Request Callback</h2>
+                <input
+                  className={styles.input__name}
+                  type="name"
+                  placeholder="Enter your name"
+                  name="name"
+                  value={values.name}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                />
+                {errors.name && touched.name ? (
+                  <div className={styles.error__container}>
+                    <svg className={styles.worning__svg}>
+                      <use href={svgPath.worning + "#worning"}></use>
+                    </svg>
+                    <p className={styles.error}>{`${errors.email}`}</p>
+                  </div>
+                ) : (
+                  <span className={styles.default}></span>
+                )}
+                <input
+                  type="email"
+                  className={styles.input__email}
+                  placeholder="Enter email*"
+                  name="email"
+                  value={values.email}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                />
+                {errors.email && touched.email ? (
+                  <div className={styles.error__container}>
+                    <svg className={styles.worning__svg}>
+                      <use href={svgPath.worning + "#worning"}></use>
+                    </svg>
+                    <p className={styles.error}>{`${errors.email}`}</p>
+                  </div>
+                ) : (
+                  <span className={styles.default}></span>
+                )}
+                <button type="submit" className={styles.from__button}>
+                  Send
+                </button>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </section>
+    </>
   );
 };
 
---
+++ src/components/form/modal/Modal.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import styles from "./Modal.module.css";
+
+import { RemoveScroll } from "react-remove-scroll";
+
+interface IModal {
+  setTumbler: Function;
+  name: string;
+  email: string;
+}
+
+const modalRoot: any = document.querySelector("#modal__root");
+
+const Modal: React.FC<IModal> = ({ setTumbler, name, email }) => {
+  const handleClick = () => {
+    setTumbler(false);
+    window.scrollTo(0, 0);
+  };
+
+  return createPortal(
+    <RemoveScroll>
+      <div className={styles.overlay}>
+        <div className={styles.modal}>
+          <p className={styles.text__modal}>Your form submission:</p>
+          <p className={styles.text__modal}>Name - {name}</p>
+          <p className={styles.text__modal}>Email - {email}</p>
+          <button
+            className={styles.modal__button}
+            type="button"
+            onClick={handleClick}
+          >
+            OK
+          </button>
+        </div>
+      </div>
+    </RemoveScroll>,
+    modalRoot
+  );
+};
+
+export default Modal;
